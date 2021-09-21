62025f151921982d1c0bbd93e3c2864783c4c600
Florian <floriandietrich1998@gmail.com>
2021-09-21 12:53:31 +0100

    retry

--- setup.py
+++ setup.py
@@ -14,10 +14,9 @@ setup(
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=[], # add any additional packages that 
+        install_requires=["cython", "mdtraj", "numpy", "scipy", "MDAnalysis"], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
-        
-        keywords=['python', 'first package'],
+    
         classifiers= [
             "Development Status :: pre - Alpha",
             "Intended Audience :: Academic",

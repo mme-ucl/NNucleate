8c7ca72d9836a3c05a0c382040e8ce0dd5e66ff9
Florian Dietrich <florian@RH-ucecfmd.chemeng.ucl.ac.uk>
2022-07-07 17:55:30 +0100

    Added gnn_train

--- NNucleate/trainig.py
+++ NNucleate/trainig.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def train(dataloader, model, loss_fn, optimizer, device, print_batch=1000000):
+def train_linear(dataloader, model, loss_fn, optimizer, device, print_batch=1000000):
     """Performs one training epoch for a NNCV.
 
     Args:
@@ -35,6 +35,55 @@ def train(dataloader, model, loss_fn, optimizer, device, print_batch=1000000):
     return loss.item()
 
 
+def train_egnn(loader, n_at, optimizer, loss_l1, model_1, device):
+    """Function to perform one epoch of a GNN training.
+
+    Args:
+        loader (torch.utils.data.Dataloader): Wrapper around a GNNTrajectory dataset.
+        n_at (int): Number of nodes per frame.
+        optimizer (torch.optim): The optimizer object for the training.
+        loss_l1 (torch.nn._Loss): Loss function for the training.
+        model_1 (GNNCV): Graph-based model to be trained.
+        device (str): Device that the training is performed on. (Required for GPU compatibility)
+
+    Returns:
+        float: Training loss at the end of the epoch.
+    """
+    res = {'loss': 0, 'counter': 0}
+    for batch, (X, y, r, c) in enumerate(loader):
+        model_1.train()
+        optimizer.zero_grad()
+        batch_size = len(X)
+        atom_positions = X.view(-1, 3).to(device)
+
+        row_new = r.view(-1)
+        col_new = c.view(-1)
+        row_new = row_new[row_new > 0]
+        col_new = col_new[col_new > 0]
+        j = 0
+        for i in range(1, len(row_new)):
+            row_new[i] += j*n_at
+            col_new[i] += j*n_at
+
+            if row_new[i-1] > row_new[i]:
+                j += 1
+                row_new[i] += n_at
+                col_new[i] += n_at
+
+        edges = [row_new.long().to(device), col_new.long().to(device)]
+        label = y.to(device)
+
+        pred = model_1(x=atom_positions, edges=edges, n_nodes = n_at)
+        loss = loss_l1(pred, label)
+        loss.backward()
+        optimizer.step()
+
+        res['loss'] += loss.item() * batch_size
+        res['counter'] += batch_size
+
+    return res['loss'] / res['counter']
+
+
 def train_perm(dataloader, model, loss_fn, optimizer, n_trans, device, print_batch=1000000):
     """Performs one training epoch for a NNCV but the loss for each batch is not just calculated on one reference structure but a set of n_trans permutated versions of that structure.
 
@@ -64,7 +113,7 @@ def train_perm(dataloader, model, loss_fn, optimizer, n_trans, device, print_bat
             loss += loss_fn(pred.flatten(), y)
 
         loss /= n_trans
-        # print(loss.item())
+
         # Backpropagation
         optimizer.zero_grad()
         loss.backward()
